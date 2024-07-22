ff970892c89aef9bdf2335a80c6dac080202b933
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-07-22 19:19:07 +0300

    Налаштовано відображення сторінки за адресою локалхосту. Додано почтакову розмітку base.html .

--- istore/core/views.py
+++ istore/core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'core/base.html') # Потім змінити на файл index.html в якому буде контент головнгої сторінки
