b083a0d7534543b6be3ee2a744b6d02589734a80
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-07-23 14:29:20 +0300

    Додано footer. Розділено файли base.html і index.html

--- istore/core/views.py
+++ istore/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'core/base.html') # Потім змінити на файл index.html в якому буде контент головнгої сторінки
+    return render(request, 'core/index.html')
