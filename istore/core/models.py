8751750207ebd712ddebbde625d5e60f4cdeb451
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-07-24 13:11:37 +0300

    Додано моделі бази даних для category, cases, headphones та заповнено початковими даними

--- istore/core/models.py
+++ istore/core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-class Product(models.Model):
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Headphones(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
@@ -20,6 +29,22 @@ class Product(models.Model):
     connection_type = models.CharField(max_length=70, blank=True, null=True)
     frequency_range = models.CharField(max_length=50, blank=True, null=True)
     sensitivity = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    image = models.ImageField(upload_to='proudcts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Cases(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    weight = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    material = models.CharField(max_length=255, blank=True, null=True)
     image = models.ImageField(upload_to='proudcts/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
