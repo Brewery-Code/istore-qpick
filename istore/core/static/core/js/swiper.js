fb602fc919779f8cd303a70aab65a6d34aeead1d
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-07-31 10:34:14 +0300

    Адаптивність головної сторінки на екранах 1200-992px

--- istore/core/static/core/js/swiper.js
+++ istore/core/static/core/js/swiper.js
@@ -2,13 +2,14 @@ var swiper = new Swiper(".swiper", {
       effect: "coverflow",
       grabCursor: true,
       centeredSlides: true,
-      slidesPerView: "auto",
+      slidesPerView: 2.5,
+      spaceBetween: 5,
       coverflowEffect: {
         rotate: 50,
         stretch: 0,
         depth: 100,
         modifier: 1,
-        slideShadows: true,
+        slideShadows: false,
       },
       pagination: {
         el: ".swiper-pagination",
