4d62c01d03467ae36e3fa244f609883909444b1f
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-08-08 09:24:28 +0300

    Винесено статичні файли в корінь проєкту

--- istore/cart/urls.py
+++ istore/cart/urls.py
@@ -5,4 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     
+    path('cart-add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('cart-change/<int:product_id>/', views.cart_change, name='cart_change'),
+    path('cart-remove/<int:product_id>/', views.cart_remove, name='cart_remove')
 ]
