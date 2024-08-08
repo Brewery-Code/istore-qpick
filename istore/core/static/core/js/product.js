c35b0e885326e7322cb70718cb1807e128c10d53
Vitalii <x4gm9k@gmail.com>
2024-08-08 12:27:30 +0300

    Add response form at the product page

--- istore/core/static/core/js/product.js
+++ istore/core/static/core/js/product.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.querySelectorAll('.description').forEach(description => {
     description.addEventListener('click', function (event) {
-        if (event.target.closest('.description')) {
+        if (event.target.closest('summary')) {
             const arrow = description.querySelector('.description__title-arrow');
             if (arrow) {
                 arrow.classList.toggle('description__title-arrow_active');
@@ -29,3 +29,36 @@ document.querySelectorAll('.description').forEach(description => {
         }
     });
 });
+
+
+let rating = document.querySelector('.product__response-number').innerHTML;
+rating = Math.round(rating);
+document.querySelectorAll('.product__response').forEach(item => {
+    item.setAttribute('data-total-value', rating);
+});
+
+
+
+const userResponsMenu = document.querySelector('.user-response');
+
+function openResponsMenu(event) {
+    userResponsMenu.style.display = 'flex';
+    setTimeout(() => {
+        userResponsMenu.style.opacity = '1';
+    }, 10);
+}
+
+function closeResponsMenu(event) {
+    if (event.target === userResponsMenu) {
+        userResponsMenu.style.display = 'none';
+        userResponsMenu.style.opacity = '0';
+    }
+}
+
+document.getElementById('user-response-btn').addEventListener('click', openResponsMenu);
+userResponsMenu.addEventListener('click', closeResponsMenu);
+userResponsMenu.querySelector('.user-response__close').addEventListener('click', () => {
+    userResponsMenu.style.display = 'none';
+    userResponsMenu.style.opacity = '0';
+    console.log('test')
+})
